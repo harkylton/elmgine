c5d5d2fba308a5f250c46c2456e83ec279895967
Viktor Holmqvist <viktorholmqvist@gmail.com>
2017-05-07 15:33:31 -0400

    First working version
    - Use canvas based Collage instead of svg, svg it's great for moving stuff
    - Add materials with density and restitution to calculate mass and inverse mass based on volume
    - Fix bugs in collision detection
    - Use AnimationFrame to get time diff for integration
    - Implement simple object integration (euler)
    - Implement simple narrow phase collison detection / resolution

--- Main.elm
+++ Main.elm
@@ -6,25 +6,108 @@ import Html.Events exposing (onClick)
 import Html.Attributes exposing (style)
 import List exposing (map)
 import Time exposing (Time, millisecond)
-import Svg exposing (Svg)
-import Svg.Attributes as SvgAttr
-import Math.Vector2 as V2 exposing(Vec2, vec2, getX, getY, scale, sub, add, dot, length, lengthSquared)
+import Math.Vector2 as V2 exposing(Vec2, vec2, getX, getY, scale, sub, add, dot, length, lengthSquared, normalize)
+import Debug
+import Keyboard
 
-type Msg = Update Time
+import AnimationFrame
+import Collage exposing (Form)
+import Color exposing (Color)
+import Element exposing (toHtml)
 
-type alias Model = { entities: List (Object Circle)
-                   , lastFrame: Time
-                   , accumulator: Float
+type Msg = Update Time | KeyDown Int
+
+type alias Model = { bodies: List RigidBody
+                   , paused: Bool
                    }
 
-type alias Object a = { a | position: Vec2, velocity: Vec2, restitution: Float, invmass: Float }
+--type alias Object a = { a | position: Vec2, velocity: Vec2, restitution: Float, invmass: Float }
 
-type alias Manifold a b = { a: Object a, b: Object b, penetration: Float, normal: Vec2 }
-type alias Rect = { min: Vec2, max: Vec2 }
-type alias Circle = { radius: Float }
+--type alias Manifold a b = { a: Object a, b: Object b, penetration: Float, normal: Vec2 }
+--type alias Rect = { min: Vec2, max: Vec2 }
+--type alias Circle = { radius: Float }
 
---type Shape = Circle { r: Float }
---| Rect { min: Vec2, max: Vec2 }
+type alias Manifold = (Float, Vec2)
+
+type Shape = Circle Float
+           | Rect Float Float
+
+type alias AABB = { min: Vec2, max: Vec2 }
+
+type alias RigidBody = { shape: Shape
+                       , material: Material
+                       , position: Vec2
+                       , velocity: Vec2
+                       --, acceleration: Vec2
+                       --, angularVelocity: Float
+                       --, orientation: Float
+                       --, torque: Float
+                       , color: Color
+                       }
+
+type Material = Rock
+              | Wood
+              | Metal
+              | Bouncy
+              | SuperBouncy
+              | Pillow
+              | Static
+              | Custom { d: Float, r: Float }
+
+
+bodyToAABB : RigidBody -> AABB
+bodyToAABB { shape, position } =
+  let x = getX position
+      y = getY position
+  in case shape of
+    Circle r -> {
+                  min = vec2 (x - r) (y - r)
+                , max = vec2 (x + r) (y + r)
+                }
+    Rect width height -> {
+                           min = vec2 (x - width / 2) (y - height / 2)
+                         , max = vec2 (x + width / 2) (y + height / 2)
+                         }
+
+density : Material -> Float
+density m = case m of
+  Rock -> 0.6
+  Wood -> 0.3
+  Metal -> 1.2
+  Bouncy -> 0.3
+  SuperBouncy -> 0.3
+  Pillow -> 0.1
+  Static -> 0
+  Custom { d } -> d
+
+
+restitution : Material -> Float
+restitution m = case m of
+  Rock -> 0.1
+  Wood -> 0.2
+  Metal -> 0.05
+  Bouncy -> 0.8
+  SuperBouncy -> 0.95
+  Pillow -> 0.2
+  Static -> 0.4
+  Custom { r } -> r
+
+volume : Shape -> Float
+volume s = case s of
+  Circle r ->
+    r^2 * pi
+  Rect width height ->
+    width * height
+
+mass : RigidBody -> Float
+mass body = volume body.shape * density body.material
+
+invmass : RigidBody -> Float
+invmass body =
+  let m = mass body
+  in if m == 0 then 0 else 1 / mass body
+
+gravity = vec2 0 -9.82
 
 main =
   program {
@@ -34,90 +117,111 @@ main =
           , subscriptions = subscriptions
           }
 
-initEntities = [{ position = vec2 50 50, radius = 10, velocity = vec2 0 0, invmass = 0, restitution = 0.1 }]
+initBodies = [
+  {
+    shape = Circle 20
+  , position = vec2 50 50
+  , velocity = vec2 0 0
+  , material = Bouncy
+  , color = Color.blue
+  },
+  {
+    shape = Rect 50 30
+  , position = vec2 -50 50
+  , velocity = vec2 0 0
+  , material = SuperBouncy
+  , color = Color.red
+  },
+  {
+    shape = Rect 25 25
+  , position = vec2 -50 100
+  , velocity = vec2 0 0
+  , material = Bouncy
+  , color = Color.yellow
+  },
+  {
+    shape = Rect 250 50
+  , position = vec2 0 -100
+  , velocity = vec2 0 0
+  , material = Static
+  , color = Color.green
+  }
+  ]
 
-
-fps = 100
-dt = 1 / fps
-accThreshold = 0.2
 
 init : (Model, Cmd Msg)
-init = ({ entities = initEntities, lastFrame = 0, accumulator = 0 }, Cmd.none)
+init = ({ bodies = initBodies, paused = False }, Cmd.none)
 
-
-toSvg : Object Circle -> Svg Msg
-toSvg e = case e of
-  { position, radius } ->
-    let x = toString (getX position)
-        y = toString (getY position)
-        r = toString radius
-    in Svg.circle [SvgAttr.cx x, SvgAttr.cy y, SvgAttr.r r, SvgAttr.fill "#0B79CE"] []
-
-
-toSvgs : List (Object Circle) -> List (Svg Msg)
-toSvgs es = map toSvg es
-
-
-svgAttr : List (Svg.Attribute Msg)
-svgAttr = [ SvgAttr.viewBox "0 0 100 100"
-          , SvgAttr.width "500px"
-          , SvgAttr.height "500px"
-          , style [("border", "1px solid black")]
-          ]
-
-
-genSvg : List (Object Circle) -> Html Msg
-genSvg es = Svg.svg svgAttr (map toSvg es)
+bodyToForm : RigidBody -> Form
+bodyToForm { shape, position, color } = case shape of
+  Circle r ->
+    Collage.circle r
+      |> Collage.filled color
+      |> Collage.move (getX position, getY position)
+  Rect width height ->
+    Collage.rect width height
+      |> Collage.filled color
+      |> Collage.move(getX position, getY position)
 
 
 view : Model -> Html Msg
-view { entities } =
-  div []
+view { bodies } =
+  div [ style [("border", "1px solid #ddd"), ("display", "inline-block")] ]
     [
-      genSvg entities
+      Collage.collage 500 500 (map bodyToForm bodies) |> toHtml
     --, button [ onClick AddCircle ] [ text "Add Circle" ]
     ]
 
 
 subscriptions : Model -> Sub Msg
-subscriptions model = Time.every millisecond Update
+subscriptions model = Sub.batch [ Keyboard.downs KeyDown, AnimationFrame.diffs Update ]
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Update t -> if model.lastFrame == 0 then
-      ({ model | lastFrame = t }, Cmd.none)
-    else
-      (updateGame t model, Cmd.none)
+    Update t -> (if model.paused then model else updatePhysics (Time.inSeconds t) model, Cmd.none)
+    KeyDown 80 -> Debug.log "paused" ({ model | paused = not model.paused }, Cmd.none)
+    KeyDown _ -> (model, Cmd.none)
 
 
-updateGame : Time -> Model -> Model
-updateGame t model = stepPhysics (model.accumulator + (Time.inSeconds t - Time.inSeconds model.lastFrame)) model
-
-
-stepPhysics : Float -> Model -> Model
-stepPhysics acc model =
-  if acc > accThreshold then
-    stepPhysics accThreshold (updatePhysics dt model)
-  else if acc > dt then
-    stepPhysics (acc - dt) (updatePhysics dt model)
-  else
-    model
 
 updatePhysics : Float -> Model -> Model
-updatePhysics dt model = model
+updatePhysics dt model = { model | bodies = (handleCollisions (map (updateBodyEuler dt) model.bodies)) }
 
-satRect : Rect -> Rect -> Bool
-satRect a b =
-  if (getX a.max) < (getX b.min) || (getX a.min) > (getX b.max) then
-    False
-  else if (getY a.max) < (getY b.min) || (getY a.min) > (getY b.max) then
-    False
-  else
-    True
+force body = vec2 0 0
 
-extent g o = (g o.max - g o.min) / 2
+getAcceleration : RigidBody -> Vec2
+getAcceleration body =
+  let im = invmass body
+  in
+      if invmass body == 0 then
+        vec2 0 0
+      else
+        add gravity (scale im (force body))
+
+updateBodyEuler : Float -> RigidBody -> RigidBody
+updateBodyEuler dt body =
+  let velocity = scale dt (getAcceleration body) |> add body.velocity
+      position = scale dt velocity |> add body.position
+  in { body | velocity = velocity, position = position }
+
+updateBody : Float -> RigidBody -> RigidBody
+updateBody dt body =
+  let acceleration = getAcceleration body
+      position = add body.position (scale dt (add velocity (scale (dt / 2) acceleration)))
+      velocity = add body.velocity (scale dt acceleration)
+      newAcceleration = getAcceleration body
+  in { body | velocity = add velocity (scale (dt / 2) (sub newAcceleration acceleration)), position = position }
+
+--satRect : Rect -> Rect -> Bool
+--satRect a b =
+--  if (getX a.max) < (getX b.min) || (getX a.min) > (getX b.max) then
+--    False
+--  else if (getY a.max) < (getY b.min) || (getY a.min) > (getY b.max) then
+--    False
+--  else
+--    True
 
 --circleCircleCollision : Circle -> Circle -> Bool
 --circleCircleCollision a b =
@@ -128,83 +232,144 @@ extent g o = (g o.max - g o.min) / 2
 --      by = getY b.position
 --  in r < (ax + bx)^2 + (ay + by)^2
 
-rectRectCollision : Object Rect -> Object Rect -> Maybe (Manifold Rect Rect)
-rectRectCollision a b =
-  let n = sub b.position a.position
+type alias CircleSpec = (Float, Vec2)
+type alias RectSpec = (AABB, Vec2)
+
+handleCollisions : List RigidBody -> List RigidBody
+handleCollisions bodies = case bodies of
+  [] -> []
+  (body::rest) ->
+    let (body_resolved, rest_resolved) = handleSingle body rest
+    in body_resolved::(handleCollisions rest_resolved)
+
+handleSingle : RigidBody -> List RigidBody -> (RigidBody, List RigidBody)
+handleSingle body bodies = case bodies of
+  [] -> (body, [])
+  (other::rest) -> case collision body other of
+    Just manifold ->
+      let (body_resolved, other_resolved) = resolveCollision body other manifold
+          (body_resolved_all, rest_resolved) = handleSingle body_resolved rest
+      in (body_resolved_all, other_resolved::rest_resolved)
+    Nothing ->
+      let (body_resolved, rest_resolved) = handleSingle body rest
+      in (body_resolved, other::rest_resolved)
+
+--handleSingleRec : RigidBody -> List RigidBody -> List RigidBody
+--handleSingleRec a bodies = case bodies of
+--  [] -> []
+--  (b:bs) -> case collision a b of
+--    Just m ->
+--      let (a', b') = resolveCollision a b m
+--      in b'::(handleSingle a' bs)
+--    Nothing -> b::(handleSingle a bs)
+
+--bodyCollisions : RigidBody -> List RigidBody -> (List (RigidBody, Manifold), List RigidBody)
+--bodyCollisions a bodies = case bodies of
+--  [] -> ([], [])
+--  (b::bs) -> case collision a b of
+--    Just m -> (a, b, m)::(bodyCollisions a bs)
+--    Nothing -> bodyCollisions a bs
+
+collision : RigidBody -> RigidBody -> Maybe Manifold
+collision a b = case a.shape of
+  Circle r -> case b.shape of
+    Circle r2 -> circleCircleCollision (r, a.position) (r2, b.position)
+    Rect _ _ -> circleRectCollision (r, a.position) (bodyToAABB b, b.position)
+  Rect _ _ -> case b.shape of
+    Circle r -> rectCircleCollision (bodyToAABB a, a.position) (r, b.position)
+    Rect _ _ -> rectRectCollision (bodyToAABB a, a.position) (bodyToAABB b, b.position)
+
+extent g o = (g o.max - g o.min) / 2
+
+rectRectCollision : RectSpec -> RectSpec -> Maybe Manifold
+rectRectCollision (a, apos) (b, bpos) =
+  let n = sub bpos apos
       x_overlap = extent getX a + extent getX b - abs (getX n)
       y_overlap = extent getY a + extent getY b - abs (getY n)
   in
       if x_overlap > 0 && y_overlap > 0 then
-        if x_overlap > y_overlap then
-          Just { a = a, b = b, penetration = x_overlap, normal = vec2 (if getX n < 0 then -1 else 1) 0 }
+        if x_overlap < y_overlap then
+          Just (x_overlap, vec2 (if getX n < 0 then -1 else 1) 0)
         else
-          Just { a = b, b = b, penetration = y_overlap, normal = vec2 0 (if getY n < 0 then -1 else 1) }
+          Just (y_overlap, vec2 0 (if getY n < 0 then -1 else 1))
       else
         Nothing
 
-resolveClosest : Object Rect -> Object Circle -> Vec2 -> (Bool, Vec2)
-resolveClosest a b n =
-  let x_extent = extent getX a
-      y_extent = extent getY a
-      closest = vec2 (clamp -x_extent x_extent (getX n)) (clamp -y_extent y_extent (getY n))
+resolveClosest : AABB -> Vec2 -> (Bool, Vec2)
+resolveClosest box normal =
+  let x_extent = extent getX box
+      y_extent = extent getY box
+      closest = vec2 (clamp -x_extent x_extent (getX normal)) (clamp -y_extent y_extent (getY normal))
   in
-      if closest /= n then
+      if closest /= normal then
         (False, closest)
       else
-        if abs (getX n) > abs (getY n) then
+        if abs (getX normal) < abs (getY normal) then
           (True, vec2 (if getX closest > 0 then x_extent else -x_extent) (getY closest))
         else
           (True, vec2 (getX closest) (if getY closest > 0 then y_extent else -y_extent))
 
+circleRectCollision : CircleSpec -> RectSpec -> Maybe Manifold
+circleRectCollision c r = case rectCircleCollision r c of
+  Nothing -> Nothing
+  Just (penetration, normal) -> Just (penetration, V2.negate normal)
 
-rectCircleCollision : Object Rect -> Object Circle -> Maybe (Manifold Rect Circle)
-rectCircleCollision a b =
-  let n = sub b.position a.position
-      (is_inside, closest) = resolveClosest a b n
+rectCircleCollision : RectSpec -> CircleSpec -> Maybe Manifold
+rectCircleCollision (box, apos) (r, bpos) =
+  let n = sub bpos apos
+      (is_inside, closest) = resolveClosest box n
       normal = sub n closest
-      r = b.radius
+      d = lengthSquared normal
   in
-      if (lengthSquared normal) > r^2 && not is_inside then
+      if d > r^2 && not is_inside then
         Nothing
       else
-        let d = length normal
-        in Just { a = a, b = b, penetration = r - d, normal = if is_inside then V2.negate n else n }
+        let l = sqrt d
+            n2 = normalize normal |> Debug.log "normal"
+        in Just (r - l, if is_inside then V2.negate n2 else n2) |> Debug.log "result"
 
 
-circleCircleCollision : Object Circle -> Object Circle -> Maybe (Manifold Circle Circle)
-circleCircleCollision a b =
-  let n = sub b.position a.position
-      r = (a.radius + b.radius)^2
-  in if lengthSquared n > r then Nothing else
-    let d = length n
-    in if d == 0 then Just { a = a, b = b, penetration = a.radius, normal = vec2 1 0 }
-       else Just { a = a, b = b, penetration = r - d, normal = scale (1 / d) n }
+circleCircleCollision : CircleSpec -> CircleSpec -> Maybe Manifold
+circleCircleCollision (ar, apos) (br, bpos) =
+  let n = sub bpos apos
+      r = (ar + br)^2
+  in
+      if lengthSquared n > r then
+        Nothing
+      else
+        let d = length n in
+          if d == 0 then
+            Just (ar, vec2 1 0)
+          else
+            Just (r - d, scale (1 / d) n)
 
 
-correctPosition : Object a -> Object b -> Float -> Vec2 -> (Object a, Object b)
-correctPosition a b penetration normal =
-  let percent = 0.2
-      slop = 0.01
-      c = max (penetration - slop) 0 / (a.invmass + b.invmass) * percent
+correctPosition : RigidBody -> RigidBody -> Manifold -> (RigidBody, RigidBody)
+correctPosition a b (penetration, normal) =
+  let percent = 0.8
+      slop = 0.02
+      c = ((max (penetration - slop) 0) / (invmass a + invmass b)) * percent
       correction = scale c normal
   in
-    ( { a | position = sub a.position (scale a.invmass correction) }
-    , { b | position = add b.position (scale b.invmass correction) }
+    ( { a | position = sub a.position (scale (invmass a) correction) }
+    , { b | position = add b.position (scale (invmass b) correction) }
     )
 
 
-resolveCollision : Manifold a b -> (Object a, Object b)
-resolveCollision { a, b, penetration, normal } =
-  let relativeVelocity = sub b.velocity a.velocity
+resolveCollision : RigidBody -> RigidBody -> Manifold -> (RigidBody, RigidBody)
+resolveCollision a b m =
+  let normal = Tuple.second m |> Debug.log "normal"
+      relativeVelocity = sub b.velocity a.velocity
       normalVelocity = dot relativeVelocity normal
-      e = min a.restitution b.restitution
-      j = -(1 + e) * normalVelocity / (a.invmass + b.invmass)
-      impulse = scale j normal
+      e = min (restitution a.material) (restitution b.material)
+      j = -(1 + e) * normalVelocity / (invmass a + invmass b)
+      impulse = scale j normal |> Debug.log "impulse"
   in
       if normalVelocity > 0 then
-        (a, b)
+        correctPosition a b m
       else
-        ( { a | velocity = sub a.velocity (scale a.invmass impulse) }
-        , { b | velocity = sub b.velocity (scale a.invmass impulse) }
-        )
+        correctPosition
+          { a | velocity = sub a.velocity (scale (invmass a) impulse) |> Debug.log "anew" }
+          { b | velocity = add b.velocity (scale (invmass b) impulse) |> Debug.log "bnew" }
+          m
 
